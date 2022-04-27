6c95dd29c12b76cb2cdcafa7d5700df8d02d1c99
Derek Bradshaw <derekbradshaw01@gmail.com>
2022-04-26 22:45:22 -0400

    Add files via upload

---
+++ Graphics/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblText = New System.Windows.Forms.Label()
+        Me.btnDrawText = New System.Windows.Forms.Button()
+        Me.txtString = New System.Windows.Forms.TextBox()
+        Me.btnDrawCurve = New System.Windows.Forms.Button()
+        Me.txtY4Curve = New System.Windows.Forms.TextBox()
+        Me.txtX4Curve = New System.Windows.Forms.TextBox()
+        Me.txtY3Curve = New System.Windows.Forms.TextBox()
+        Me.txtX3Curve = New System.Windows.Forms.TextBox()
+        Me.txtY2Curve = New System.Windows.Forms.TextBox()
+        Me.txtX2Curve = New System.Windows.Forms.TextBox()
+        Me.txtY1Curve = New System.Windows.Forms.TextBox()
+        Me.txtX1Curve = New System.Windows.Forms.TextBox()
+        Me.txtY2 = New System.Windows.Forms.TextBox()
+        Me.txtY1 = New System.Windows.Forms.TextBox()
+        Me.txtX2 = New System.Windows.Forms.TextBox()
+        Me.txtX1 = New System.Windows.Forms.TextBox()
+        Me.btnRectGrad = New System.Windows.Forms.Button()
+        Me.btnRectHatch = New System.Windows.Forms.Button()
+        Me.btnRectangle = New System.Windows.Forms.Button()
+        Me.btnEllipse = New System.Windows.Forms.Button()
+        Me.btnFontSelect = New System.Windows.Forms.Button()
+        Me.btnColorSelect = New System.Windows.Forms.Button()
+        Me.diaColor = New System.Windows.Forms.ColorDialog()
+        Me.diaFont = New System.Windows.Forms.FontDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox1.Controls.Add(Me.lblText)
+        Me.GroupBox1.Controls.Add(Me.btnDrawText)
+        Me.GroupBox1.Controls.Add(Me.txtString)
+        Me.GroupBox1.Controls.Add(Me.btnDrawCurve)
+        Me.GroupBox1.Controls.Add(Me.txtY4Curve)
+        Me.GroupBox1.Controls.Add(Me.txtX4Curve)
+        Me.GroupBox1.Controls.Add(Me.txtY3Curve)
+        Me.GroupBox1.Controls.Add(Me.txtX3Curve)
+        Me.GroupBox1.Controls.Add(Me.txtY2Curve)
+        Me.GroupBox1.Controls.Add(Me.txtX2Curve)
+        Me.GroupBox1.Controls.Add(Me.txtY1Curve)
+        Me.GroupBox1.Controls.Add(Me.txtX1Curve)
+        Me.GroupBox1.Controls.Add(Me.txtY2)
+        Me.GroupBox1.Controls.Add(Me.txtY1)
+        Me.GroupBox1.Controls.Add(Me.txtX2)
+        Me.GroupBox1.Controls.Add(Me.txtX1)
+        Me.GroupBox1.Controls.Add(Me.btnRectGrad)
+        Me.GroupBox1.Controls.Add(Me.btnRectHatch)
+        Me.GroupBox1.Controls.Add(Me.btnRectangle)
+        Me.GroupBox1.Controls.Add(Me.btnEllipse)
+        Me.GroupBox1.Controls.Add(Me.btnFontSelect)
+        Me.GroupBox1.Controls.Add(Me.btnColorSelect)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(165, 565)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GDI + Primatives"
+        '
+        'lblText
+        '
+        Me.lblText.AutoSize = True
+        Me.lblText.Location = New System.Drawing.Point(27, 434)
+        Me.lblText.Name = "lblText"
+        Me.lblText.Size = New System.Drawing.Size(118, 16)
+        Me.lblText.TabIndex = 22
+        Me.lblText.Text = "Text To Display"
+        '
+        'btnDrawText
+        '
+        Me.btnDrawText.Location = New System.Drawing.Point(8, 481)
+        Me.btnDrawText.Name = "btnDrawText"
+        Me.btnDrawText.Size = New System.Drawing.Size(151, 23)
+        Me.btnDrawText.TabIndex = 21
+        Me.btnDrawText.Text = "Draw Text"
+        Me.btnDrawText.UseVisualStyleBackColor = True
+        '
+        'txtString
+        '
+        Me.txtString.Location = New System.Drawing.Point(8, 453)
+        Me.txtString.Name = "txtString"
+        Me.txtString.Size = New System.Drawing.Size(151, 22)
+        Me.txtString.TabIndex = 20
+        '
+        'btnDrawCurve
+        '
+        Me.btnDrawCurve.Location = New System.Drawing.Point(8, 379)
+        Me.btnDrawCurve.Name = "btnDrawCurve"
+        Me.btnDrawCurve.Size = New System.Drawing.Size(151, 23)
+        Me.btnDrawCurve.TabIndex = 19
+        Me.btnDrawCurve.Text = "DrawCurve"
+        Me.btnDrawCurve.UseVisualStyleBackColor = True
+        '
+        'txtY4Curve
+        '
+        Me.txtY4Curve.Location = New System.Drawing.Point(129, 351)
+        Me.txtY4Curve.Name = "txtY4Curve"
+        Me.txtY4Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtY4Curve.TabIndex = 18
+        Me.txtY4Curve.Text = "Y4"
+        Me.txtY4Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtX4Curve
+        '
+        Me.txtX4Curve.Location = New System.Drawing.Point(88, 351)
+        Me.txtX4Curve.Name = "txtX4Curve"
+        Me.txtX4Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtX4Curve.TabIndex = 17
+        Me.txtX4Curve.Text = "X4"
+        Me.txtX4Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtY3Curve
+        '
+        Me.txtY3Curve.Location = New System.Drawing.Point(47, 351)
+        Me.txtY3Curve.Name = "txtY3Curve"
+        Me.txtY3Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtY3Curve.TabIndex = 16
+        Me.txtY3Curve.Text = "Y3"
+        Me.txtY3Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtX3Curve
+        '
+        Me.txtX3Curve.Location = New System.Drawing.Point(6, 351)
+        Me.txtX3Curve.Name = "txtX3Curve"
+        Me.txtX3Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtX3Curve.TabIndex = 15
+        Me.txtX3Curve.Text = "X3"
+        Me.txtX3Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtY2Curve
+        '
+        Me.txtY2Curve.Location = New System.Drawing.Point(129, 323)
+        Me.txtY2Curve.Name = "txtY2Curve"
+        Me.txtY2Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtY2Curve.TabIndex = 14
+        Me.txtY2Curve.Text = "Y2"
+        Me.txtY2Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtX2Curve
+        '
+        Me.txtX2Curve.Location = New System.Drawing.Point(88, 323)
+        Me.txtX2Curve.Name = "txtX2Curve"
+        Me.txtX2Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtX2Curve.TabIndex = 13
+        Me.txtX2Curve.Text = "X2"
+        Me.txtX2Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtY1Curve
+        '
+        Me.txtY1Curve.Location = New System.Drawing.Point(47, 323)
+        Me.txtY1Curve.Name = "txtY1Curve"
+        Me.txtY1Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtY1Curve.TabIndex = 12
+        Me.txtY1Curve.Text = "Y1"
+        Me.txtY1Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtX1Curve
+        '
+        Me.txtX1Curve.Location = New System.Drawing.Point(6, 323)
+        Me.txtX1Curve.Name = "txtX1Curve"
+        Me.txtX1Curve.Size = New System.Drawing.Size(35, 22)
+        Me.txtX1Curve.TabIndex = 11
+        Me.txtX1Curve.Text = "X1"
+        Me.txtX1Curve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtY2
+        '
+        Me.txtY2.Location = New System.Drawing.Point(97, 129)
+        Me.txtY2.Name = "txtY2"
+        Me.txtY2.Size = New System.Drawing.Size(48, 22)
+        Me.txtY2.TabIndex = 10
+        Me.txtY2.Text = "Y2"
+        Me.txtY2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtY1
+        '
+        Me.txtY1.Location = New System.Drawing.Point(97, 101)
+        Me.txtY1.Name = "txtY1"
+        Me.txtY1.Size = New System.Drawing.Size(48, 22)
+        Me.txtY1.TabIndex = 9
+        Me.txtY1.Text = "Y1"
+        Me.txtY1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtX2
+        '
+        Me.txtX2.Location = New System.Drawing.Point(8, 129)
+        Me.txtX2.Name = "txtX2"
+        Me.txtX2.Size = New System.Drawing.Size(48, 22)
+        Me.txtX2.TabIndex = 8
+        Me.txtX2.Text = "X2"
+        Me.txtX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtX1
+        '
+        Me.txtX1.Location = New System.Drawing.Point(8, 101)
+        Me.txtX1.Name = "txtX1"
+        Me.txtX1.Size = New System.Drawing.Size(48, 22)
+        Me.txtX1.TabIndex = 7
+        Me.txtX1.Text = "X1"
+        Me.txtX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnRectGrad
+        '
+        Me.btnRectGrad.Location = New System.Drawing.Point(6, 244)
+        Me.btnRectGrad.Name = "btnRectGrad"
+        Me.btnRectGrad.Size = New System.Drawing.Size(137, 44)
+        Me.btnRectGrad.TabIndex = 6
+        Me.btnRectGrad.Text = "Rectangle Gradient"
+        Me.btnRectGrad.UseVisualStyleBackColor = True
+        '
+        'btnRectHatch
+        '
+        Me.btnRectHatch.Location = New System.Drawing.Point(6, 215)
+        Me.btnRectHatch.Name = "btnRectHatch"
+        Me.btnRectHatch.Size = New System.Drawing.Size(137, 23)
+        Me.btnRectHatch.TabIndex = 5
+        Me.btnRectHatch.Text = "Rectangle Hatch"
+        Me.btnRectHatch.UseVisualStyleBackColor = True
+        '
+        'btnRectangle
+        '
+        Me.btnRectangle.Location = New System.Drawing.Point(8, 186)
+        Me.btnRectangle.Name = "btnRectangle"
+        Me.btnRectangle.Size = New System.Drawing.Size(137, 23)
+        Me.btnRectangle.TabIndex = 4
+        Me.btnRectangle.Text = "Draw Rectangle"
+        Me.btnRectangle.UseVisualStyleBackColor = True
+        '
+        'btnEllipse
+        '
+        Me.btnEllipse.Location = New System.Drawing.Point(8, 157)
+        Me.btnEllipse.Name = "btnEllipse"
+        Me.btnEllipse.Size = New System.Drawing.Size(137, 23)
+        Me.btnEllipse.TabIndex = 3
+        Me.btnEllipse.Text = "Draw Ellipse"
+        Me.btnEllipse.UseVisualStyleBackColor = True
+        '
+        'btnFontSelect
+        '
+        Me.btnFontSelect.Location = New System.Drawing.Point(8, 62)
+        Me.btnFontSelect.Name = "btnFontSelect"
+        Me.btnFontSelect.Size = New System.Drawing.Size(137, 23)
+        Me.btnFontSelect.TabIndex = 2
+        Me.btnFontSelect.Text = "Select Font"
+        Me.btnFontSelect.UseVisualStyleBackColor = True
+        '
+        'btnColorSelect
+        '
+        Me.btnColorSelect.Location = New System.Drawing.Point(8, 33)
+        Me.btnColorSelect.Name = "btnColorSelect"
+        Me.btnColorSelect.Size = New System.Drawing.Size(137, 23)
+        Me.btnColorSelect.TabIndex = 1
+        Me.btnColorSelect.Text = "Select Color"
+        Me.btnColorSelect.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 573)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "CPT 341 Project 4 - GDI+ Fall 2021"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnFontSelect As Button
+    Friend WithEvents btnColorSelect As Button
+    Friend WithEvents diaColor As ColorDialog
+    Friend WithEvents diaFont As FontDialog
+    Friend WithEvents btnEllipse As Button
+    Friend WithEvents btnRectangle As Button
+    Friend WithEvents btnRectGrad As Button
+    Friend WithEvents btnRectHatch As Button
+    Friend WithEvents btnDrawCurve As Button
+    Friend WithEvents txtY4Curve As TextBox
+    Friend WithEvents txtX4Curve As TextBox
+    Friend WithEvents txtY3Curve As TextBox
+    Friend WithEvents txtX3Curve As TextBox
+    Friend WithEvents txtY2Curve As TextBox
+    Friend WithEvents txtX2Curve As TextBox
+    Friend WithEvents txtY1Curve As TextBox
+    Friend WithEvents txtX1Curve As TextBox
+    Friend WithEvents txtY2 As TextBox
+    Friend WithEvents txtY1 As TextBox
+    Friend WithEvents txtX2 As TextBox
+    Friend WithEvents txtX1 As TextBox
+    Friend WithEvents lblText As Label
+    Friend WithEvents btnDrawText As Button
+    Friend WithEvents txtString As TextBox
+End Class
---
+++ Graphics/Form1.vb
@@ -0,0 +1,268 @@
+﻿Imports System.Drawing.Drawing2D
+'Derek Bradshaw
+'11/13/2021
+'CPT 341 -  VB.NET NJIT Fall 2021 Semester
+
+Public Class Form1
+    'init color as black
+    Dim pen As Pen = New Pen(Color.Black, 5)
+
+    'set position inits
+    Dim xPosAdd As Integer = 200
+    Dim yPosAdd As Integer = 10
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'Tool tip stuff
+        Dim toolTip1 As New ToolTip()
+        Dim toolTip2 As New ToolTip()
+        Dim toolTip3 As New ToolTip()
+        Dim toolTip4 As New ToolTip()
+        Dim toolTip5 As New ToolTip()
+        Dim toolTip6 As New ToolTip()
+        Dim toolTip7 As New ToolTip()
+        Dim toolTip8 As New ToolTip()
+        toolTip1.ShowAlways = True
+        toolTip2.ShowAlways = True
+        toolTip3.ShowAlways = True
+        toolTip4.ShowAlways = True
+        toolTip5.ShowAlways = True
+        toolTip6.ShowAlways = True
+        toolTip7.ShowAlways = True
+        toolTip8.ShowAlways = True
+        toolTip1.SetToolTip(btnColorSelect, "Select a Color to draw with")
+        toolTip2.SetToolTip(btnFontSelect, "Select a Font to type with")
+        toolTip3.SetToolTip(btnEllipse, "Draws an ellipse with given values")
+        toolTip4.SetToolTip(btnRectangle, "Draws an rectangle with given values")
+        toolTip5.SetToolTip(btnRectHatch, "Draws an rectangle with a hatch pattern with given values")
+        toolTip6.SetToolTip(btnRectGrad, "Draws an rectangle with a gradient pattern with given values")
+        toolTip7.SetToolTip(btnDrawCurve, "Draws a belitzer curve with given values")
+        toolTip8.SetToolTip(btnDrawText, "Displays given text with chosen color and font")
+    End Sub
+
+    Private Sub Form1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles MyBase.Paint
+        Dim g As Drawing.Graphics = e.Graphics 'Not sure why i have to do it like this, but whatever
+    End Sub
+    'Dialouge stuff
+    '---------------------------------------------------------------------------------------------------------
+
+    Private Sub btnColorSelect_Click(sender As Object, e As EventArgs) Handles btnColorSelect.Click
+        diaColor.ShowDialog()
+    End Sub
+
+    Private Sub btnFontSelect_Click(sender As Object, e As EventArgs) Handles btnFontSelect.Click
+        diaFont.ShowDialog()
+    End Sub
+    '---------------------------------------------------------------------------------------------------------
+
+    'The following call the approprite function below to draw their shape
+    '---------------------------------------------------------------------------------------------------------
+    Private Sub btnEllipse_Click(sender As Object, e As EventArgs) Handles btnEllipse.Click
+        DrawEllipse()
+    End Sub
+
+    Private Sub btnRectangle_Click(sender As Object, e As EventArgs) Handles btnRectangle.Click
+        DrawRectangle()
+    End Sub
+
+    Private Sub btnRectGrad_Click(sender As Object, e As EventArgs) Handles btnRectGrad.Click
+        DrawRectGradient()
+    End Sub
+
+    Private Sub btnRectHatch_Click(sender As Object, e As EventArgs) Handles btnRectHatch.Click
+        DrawRectHatch()
+    End Sub
+
+    Private Sub btnDrawCurve_Click(sender As Object, e As EventArgs) Handles btnDrawCurve.Click
+        DrawCurve()
+    End Sub
+
+    Private Sub btnDrawText_Click(sender As Object, e As EventArgs) Handles btnDrawText.Click
+        DrawText()
+    End Sub
+    '---------------------------------------------------------------------------------------------------------
+
+
+    'Draww the Correct shape
+    '---------------------------------------------------------------------------------------------------------
+
+    Public Sub DrawEllipse() 'Draws circle
+        Dim X1 As Decimal
+        Dim X2 As Decimal
+        Dim Y1 As Decimal
+        Dim Y2 As Decimal
+        Try 'getting X and Y 
+            X1 = CDec(txtX1.Text) + xPosAdd
+            X2 = CDec(txtX2.Text)
+            Y1 = CDec(txtY1.Text) + yPosAdd
+            Y2 = CDec(txtY2.Text)
+
+            Me.Refresh()
+            Dim b As Pen = New Pen(diaColor.Color, 10)
+            Using g As Drawing.Graphics = Me.CreateGraphics
+                'Draw a Circle 
+                g.DrawEllipse(b, X1, Y1, X2, Y2)
+            End Using
+        Catch
+            'Creates pop-up to tell user what is wrong & clears the approprite field
+            MessageBox.Show("Please enter a valid decimal value.", "Value Invalid",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtX1.Clear()
+            txtX2.Clear()
+            txtY1.Clear()
+            txtY2.Clear()
+        End Try
+    End Sub
+
+    Public Sub DrawRectangle() 'draws empty rectangle
+        Dim X1 As Decimal
+        Dim X2 As Decimal
+        Dim Y1 As Decimal
+        Dim Y2 As Decimal
+        Try 'getting X and Y 
+            X1 = CDec(txtX1.Text) + xPosAdd
+            X2 = CDec(txtX2.Text)
+            Y1 = CDec(txtY1.Text) + yPosAdd
+            Y2 = CDec(txtY2.Text)
+
+            Me.Refresh()
+            Dim b As Pen = New Pen(diaColor.Color, 10)
+            Using g As Drawing.Graphics = Me.CreateGraphics
+                'Draw a Rectangle 
+                g.DrawRectangle(b, X1, Y1, X2, Y2)
+            End Using
+        Catch
+            'Creates pop-up to tell user what is wrong & clears the approprite field
+            MessageBox.Show("Please enter a valid decimal value.", "Value Invalid",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtX1.Clear()
+            txtX2.Clear()
+            txtY1.Clear()
+            txtY2.Clear()
+        End Try
+    End Sub
+
+    Public Sub DrawRectGradient() 'drawn rectangle with gradient to blue
+        Dim X1 As Decimal
+        Dim X2 As Decimal
+        Dim Y1 As Decimal
+        Dim Y2 As Decimal
+        Try 'getting X and Y for Circle and Rectangle
+            X1 = CDec(txtX1.Text) + xPosAdd
+            X2 = CDec(txtX2.Text)
+            Y1 = CDec(txtY1.Text) + yPosAdd
+            Y2 = CDec(txtY2.Text)
+
+            Me.Refresh()
+            Dim oRect As New Rectangle(X1, Y1, X2, Y2)
+            Dim b As New LinearGradientBrush(oRect, diaColor.Color, Color.Blue,
+    LinearGradientMode.ForwardDiagonal)
+            Using g As Drawing.Graphics = Me.CreateGraphics
+
+                'Draw a Rectangle 
+                g.FillRectangle(b, X1, Y1, X2, Y2)
+            End Using
+        Catch
+            'Creates pop-up to tell user what is wrong & clears the approprite field
+            MessageBox.Show("Please enter a valid decimal value.", "Value Invalid",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtX1.Clear()
+            txtX2.Clear()
+            txtY1.Clear()
+            txtY2.Clear()
+        End Try
+
+    End Sub
+
+    Public Sub DrawRectHatch() 'draws rectangle with a hatch pattern
+        Dim X1 As Decimal
+        Dim X2 As Decimal
+        Dim Y1 As Decimal
+        Dim Y2 As Decimal
+        Try 'getting X and Y
+            X1 = CDec(txtX1.Text) + xPosAdd
+            X2 = CDec(txtX2.Text)
+            Y1 = CDec(txtY1.Text) + yPosAdd
+            Y2 = CDec(txtY2.Text)
+
+            Me.Refresh()
+            Dim oRect As New Rectangle(X1, Y1, X2, Y2)
+            Dim b As New HatchBrush(
+             HatchStyle.Weave,
+            Color.Black,
+            diaColor.Color)
+            Using g As Drawing.Graphics = Me.CreateGraphics
+
+                'Draw a Rectangle 
+                g.FillRectangle(b, X1, Y1, X2, Y2)
+            End Using
+        Catch
+            'Creates pop-up to tell user what is wrong & clears the approprite field
+            MessageBox.Show("Please enter a valid decimal value.", "Value Invalid",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtX1.Clear()
+            txtX2.Clear()
+            txtY1.Clear()
+            txtY2.Clear()
+        End Try
+    End Sub
+
+    Public Sub DrawCurve() 'draws belitzer curve
+        Dim X1 As Decimal
+        Dim X2 As Decimal
+        Dim X3 As Decimal
+        Dim X4 As Decimal
+        Dim Y1 As Decimal
+        Dim Y2 As Decimal
+        Dim Y3 As Decimal
+        Dim Y4 As Decimal
+        Try 'getting X and Y 
+            X1 = CDec(txtX1Curve.Text)
+            X2 = CDec(txtX2Curve.Text)
+            X3 = CDec(txtX3Curve.Text)
+            X4 = CDec(txtX4Curve.Text)
+            Y1 = CDec(txtY1Curve.Text)
+            Y2 = CDec(txtY2Curve.Text)
+            Y3 = CDec(txtY3Curve.Text)
+            Y4 = CDec(txtY4Curve.Text)
+
+            Me.Refresh()
+            Dim b As Pen = New Pen(diaColor.Color, 10)
+            Using g As Drawing.Graphics = Me.CreateGraphics
+                g.SmoothingMode = SmoothingMode.HighQuality
+                'Draw a Curve 
+                g.DrawBezier(b, X1, Y1, X2, Y2, X3, Y3, X4, Y4)
+            End Using
+        Catch
+            'Creates pop-up to tell user what is wrong & clears the approprite field
+            MessageBox.Show("Please enter a valid decimal value.", "Value Invalid",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtX1Curve.Clear()
+            txtX2Curve.Clear()
+            txtX3Curve.Clear()
+            txtX4Curve.Clear()
+            txtY1Curve.Clear()
+            txtY2Curve.Clear()
+            txtY3Curve.Clear()
+            txtY4Curve.Clear()
+        End Try
+
+    End Sub
+
+    Public Sub DrawText() 'displays text
+
+
+        Me.Refresh()
+        Dim b = New SolidBrush(diaColor.Color)
+
+        Using g As Drawing.Graphics = Me.CreateGraphics
+            'Draw Text
+            g.DrawString(txtString.Text, diaFont.Font, b, xPosAdd, yPosAdd)
+        End Using
+
+
+    End Sub
+
+
+End Class
